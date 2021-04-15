b4a332c8014f864e8153532f4c62b136b4b9f138
Zamora S.L <Yo>
2021-04-15 17:58:33 +0200

    Initial Commit

---
+++ i-Queue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i-Queue
+//
+//  Created by A4-iMAC09 on 15/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
